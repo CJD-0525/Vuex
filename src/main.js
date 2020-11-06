34aac686ee4d98eafdd6c75f72b9e21c71c27b31
chenjiandong <812441800@QQ.com>
2020-11-06 21:22:53 +0800

    todolist

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import Antd from 'ant-design-vue'
+// 2. 导入组件库的样式表
+import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Antd)
 
 new Vue({
   store,
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,76 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    inputValue: 'aaa',
+    nextId: 5,
+    viewKey: 'all'
   },
   mutations: {
+    initList (state, list) {
+      state.list = list
+    },
+    setInputValue (state, val) {
+      state.inputValue = val
+    },
+    addItem (state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    removeItem (state, id) {
+      const i = state.list.findIndex(x => x.id === id)
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    changeStatus (state, param) {
+      const i = state.list.findIndex(x => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    cleanDone (state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    changeViewKey (state, key) {
+      state.viewKey = key
+    }
   },
   actions: {
+    getList (context) {
+      axios.get('/list.json').then(({ data }) => {
+        // console.log(data)
+        context.commit('initList', data)
+      })
+    }
+  },
+  getters: {
+    unDoneLength (state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    infolist (state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      }
+      if (state.viewKey === 'undone') {
+        return state.list.filter(x => !x.done)
+      }
+      if (state.viewKey === 'done') {
+        return state.list.filter(x => x.done)
+      }
+      return state.list
+    }
   },
   modules: {
   }
